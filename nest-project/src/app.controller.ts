258897cc5370db6538f25d7439ae5bfe48bd9a6f
Korsukov <korsukov105@gmail.com>
2023-09-12 16:21:00 +0600

    Nest

--- nest-project/src/app.controller.ts
+++ nest-project/src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from 'express';
+import {
+  Controller,
+  Get,
+  ParseIntPipe,
+  Param,
+  Req,
+  HostParam,
+  Body,
+  Post,
+  BadRequestException,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateDto } from 'dto/create.dto';
 
-@Controller()
+@Controller('/app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/:id')
+  getParams(@Param('id', ParseIntPipe) id: number) {
+    console.log(id);
+    if (id > 2) {
+      throw new BadRequestException('ID must be > 2');
+    }
+    return id;
+  }
+ 
+  @UsePipes(new ValidationPipe())
+  @Post('create')
+  create(@Body() dto: CreateDto) { 
+    console.log(dto);
+    return dto;
   }
 }
